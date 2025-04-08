180312930e534614072ae9f45e2b727798d329ef
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-04-08 12:27:35 -0300

    feat(Domain): Order entity

--- CQRSCS.Domain/Entities/Order.cs
+++ CQRSCS.Domain/Entities/Order.cs
@@ -4,10 +4,22 @@ using CQRSCS.Domain.Validation;
 namespace CQRSCS.Domain.Entities; 
 
 public sealed class Order : Entity {
+    public Order(DateTime? date, OrderStatus status) {
+        ValidateDate(date);
+        ValidateStatus(status);
+
+        this.Date = date;
+        this.Status = status;
+    }
+
     public Order(int id, DateTime? date, OrderStatus status) {
         ValidateId(id);
         ValidateDate(date);
-        //ValidateStatus(status);
+        ValidateStatus(status);
+
+        this.Id = id;
+        this.Date = date;
+        this.Status = status;
     }
 
     public DateTime? Date { get; private set; }
@@ -15,6 +27,14 @@ public sealed class Order : Entity {
 
     public int CustomerId { get; set; }
     public Customer Customer { get; set; }
+
+    public void Update(DateTime? date, OrderStatus status) {
+        ValidateDate(date);
+        ValidateStatus(status);
+
+        this.Date = date;
+        this.Status = status;
+    }
 
     private void ValidateId(int id) {
         DomainEntityValidation.When(id < 0,
@@ -24,5 +44,8 @@ public sealed class Order : Entity {
         DomainEntityValidation.When(birthDate == null,
             "Error: birthDate is required.");
     }
-    
+    private void ValidateStatus(OrderStatus? status) {
+        DomainEntityValidation.When(status == null,
+            "Error: birthDate is required.");
+    }
 }
