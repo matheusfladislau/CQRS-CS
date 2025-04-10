091fd7bb6eadf848a5b05bb86bb209a83e689e0b
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-04-10 17:48:50 -0300

    feat(Application): added DTOs

---
+++ CQRSCS.Application/DTOs/OrderDTO.cs
@@ -0,0 +1,21 @@
+﻿using CQRSCS.Domain.Entities;
+using CQRSCS.Domain.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CQRSCS.Application.DTOs; 
+public class OrderDTO {
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Date is required.")]
+    [DisplayName("Date")]
+    public DateTime? Date { get; private set; }
+
+    [Required(ErrorMessage = "Status is required.")]
+    [DisplayName("Status")]
+    public OrderStatus Status { get; private set; }
+
+    [DisplayName("Customer")]
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; }
+}
---
+++ CQRSCS.Application/DTOs/OrderProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using CQRSCS.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CQRSCS.Application.DTOs; 
+public class OrderProductDTO {
+    [Required(ErrorMessage = "Quantity is required.")]
+    [DisplayName("Quantity")]
+    public int Quantity { get; set; }
+
+    [DisplayName("Product")]
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+
+    [DisplayName("Order")]
+    public int OrderId { get; set; }
+    public Order Order { get; set; }
+}
