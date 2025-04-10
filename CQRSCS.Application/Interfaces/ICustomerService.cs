922e3e5835bb25bb37df434151d9be3b9166f75f
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-04-10 17:49:37 -0300

    feat(Application): added services' interfaces

---
+++ CQRSCS.Application/Interfaces/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using CQRSCS.Application.DTOs;
+
+namespace CQRSCS.Application.Interfaces; 
+public interface ICustomerService {
+    Task<IEnumerable<CustomerDTO>> GetCustomers();
+    Task<CustomerDTO> GetById(int? id);
+
+    Task Add(CustomerDTO customer);
+    Task Remove(CustomerDTO customer);
+    Task Update(CustomerDTO customer);
+}
---
+++ CQRSCS.Application/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using CQRSCS.Application.DTOs;
+using CQRSCS.Domain.Enums;
+
+namespace CQRSCS.Application.Interfaces; 
+public interface IOrderService {
+    Task<IEnumerable<OrderDTO>> GetOrders();
+    Task<OrderDTO> GetById(int? id);
+    Task<OrderDTO> UpdateStatus(OrderDTO order, OrderStatus status);
+    Task<IEnumerable<OrderDTO>> GetByStatus(OrderStatus status);
+
+    Task<OrderDTO> Add(OrderDTO order);
+    Task<OrderDTO> Remove(OrderDTO order);
+    Task<OrderDTO> Update(OrderDTO order);
+}
