23af25f6d78064aaa85b9340ce85fc9539cc2ded
matheusfladislau <matheus.ladislaudesenv@gmail.com>
2025-04-11 11:30:22 -0300

    feat(Order): added service and interface

--- CQRSCS.Application/Interfaces/IOrderService.cs
+++ CQRSCS.Application/Interfaces/IOrderService.cs
@@ -5,10 +5,10 @@ namespace CQRSCS.Application.Interfaces;
 public interface IOrderService {
     Task<IEnumerable<OrderDTO>> GetOrders();
     Task<OrderDTO> GetById(int? id);
-    Task<OrderDTO> UpdateStatus(OrderDTO order, OrderStatus status);
+    Task UpdateStatus(OrderDTO order, OrderStatus status);
     Task<IEnumerable<OrderDTO>> GetByStatus(OrderStatus status);
 
-    Task<OrderDTO> Add(OrderDTO order);
-    Task<OrderDTO> Remove(OrderDTO order);
-    Task<OrderDTO> Update(OrderDTO order);
+    Task Add(OrderDTO order);
+    Task Remove(OrderDTO order);
+    Task Update(OrderDTO order);
 }
